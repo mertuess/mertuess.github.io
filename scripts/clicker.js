bc898ac79ecfb96ed0eaea5aed35e33dbd7b3602
mertuess <folsonner@yandex.ru>
2022-04-26 03:00:58 +0300

    Added mini-games, music player

---
+++ scripts/clicker.js
@@ -0,0 +1,39 @@
+var coins = 0;
+var coins_counter = document.getElementById('coins-counter');
+var coin = document.getElementById('coin-img-tag');
+var coin_plus = document.getElementById('coins-counter-1');
+
+var pops = document.getElementsByClassName('pop');
+
+function addCoins(){
+    coins++;
+    if(!coins_counter.classList.contains('_active')){
+        coins_counter.classList.add('_active');
+    }
+    coins_counter.innerText = '' + coins;
+    if(!coin.classList.contains('_pressed')){
+        coin.classList.add('_pressed');
+    }
+    else{
+        coin.classList.remove('_pressed');
+    }
+
+    if(!coin_plus.classList.contains('_active')){
+        coin_plus.classList.add('_active');
+    }
+    else{
+        coin_plus.classList.remove('_active');
+    }
+}
+
+function pop_it(id){
+    if(!pops[id].classList.contains('_popped')){
+        pops[id].classList.add('_popped');
+        document.getElementById('pop-snd').play();
+    }
+    else{
+        pops[id].classList.remove('_popped');
+        document.getElementById('pop-snd-1').play();
+    }
+}
+
